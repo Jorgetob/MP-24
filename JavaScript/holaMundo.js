ef17c325fafbc127be49909417de6a15b16e4968
Jorge <jorgetobosotorets@gmail.com>
2024-02-05 16:10:16 +0100

    04/02

---
+++ JavaScript/holaMundo.js
@@ -0,0 +1,21 @@
+// Voy a poner una alerta
+
+alert('Hola Mundo')
+
+//primera variable
+let calleMaster = 'Doctor Castelo'
+const velocidadluz = 123456
+
+//ejercicio utilzando variables
+
+console.log(calleMaster)
+
+//dimensiones del cubo
+var ancho = 23; // en centímetros
+var alto = 13;  // en centímetros
+var profundidad = 16; // en centímetros
+
+//calculo del volumen 
+console.log (ancho * alto * profundidad)
+
+
